d794e3315954896f0f059f3a6858cdb22e48705b
MaulizaSafonna <145952085+MaulizaSafonna@users.noreply.github.com>
2023-10-08 12:18:39 +0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,71 @@
+// Program js if else
+//const number = 90;
+//if (number >= 85) {
+//console.log("Selamat Kamu Lulus");
+//} else  {
+//   console.log("Mohon maaf,Kamu mengulang tahun depan");
+//}
+
+//Program js nested if
+//let man = "kaya";{
+//}if (man >="5.000.000.000"){
+//    console.log("Kamu laki-laki kaya dengan uang lebih dari 5 miliyar.");
+//   } if (man>="10.000.000.000"){
+//      console.log("Kamu laki-laki kaya raya dengan uang lebih dari 10 miliyar");
+//  } else {
+//    console.log("Uang kamu tidak lebih dari uang orang kaya.");
+//  }
+
+//Program js switch case
+//let adik ="Mila";
+//switch(adik) {
+//  case "Mila":
+//  case "Yorika":
+//  case "Fadli":
+//  case "Amelia":
+//    console.log("${adik}Dia adalah adik saya." );
+//    break;
+//  default:
+//    console.log("${adik} Saya tidak mempunyai adik.");
+//    break;
+//}
+
+// Program js for statement
+// const m=7;
+// for(let i =1; i<=m ;i++) {
+// console.log("Mauliza Safonna dari Aceh")
+// }
+
+//Program js While
+// let m=7;
+// while (m<=23) {
+//   console.log(m);
+//   m++;
+// }
+
+//Program js do while
+// let m=7;
+// do {
+//   console.log(m);
+//   m--;
+// }
+// while(m >=-23);
+
+//Program js Function
+// function JumlahVolumeTigakubus(m,n,o){
+//   var VolumeM;
+//   var VolumeN;
+//   var VolumeO;
+//   var total;
+// VolumeM=m*m*m;
+// VolumeN=n*n*n;
+// VolumeO=o*o*o;
+// total =VolumeM +VolumeN+VolumeO;
+//  return total;
+// }
+//  console.log(JumlahVolumeTigakubus(7,3,2));
+
+
+
+
+  
